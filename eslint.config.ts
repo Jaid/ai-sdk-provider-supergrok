41e6644cbd715523c8ae085e45976c966bd9b924
Jaid <6216144+Jaid@users.noreply.github.com>
2026-06-06 06:01:07 +0100

    Updated ESLint config
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,3 +1,39 @@
-import {makeEslintConfig} from 'eslint-config-jaid'
+import type {Linter} from 'eslint'
 
-export default makeEslintConfig()
+import {makeEslintConfig} from 'eslint-config-jaid'
+import eslintPluginZod from 'eslint-plugin-zod'
+
+const config: Array<Linter.Config> = [
+  {
+    ignores: ['dist/**', 'out/**', 'private/**', 'temp/**', 'test/lib/fixtures/content/**'],
+  },
+  ...makeEslintConfig(),
+  {
+    plugins: {
+      zod: eslintPluginZod,
+    },
+    rules: {
+      'zod/no-optional-and-default-together': 'warn',
+      'zod/array-style': 'warn',
+      'zod/no-empty-custom-schema': 'warn',
+      'zod/no-number-schema-with-finite': 'warn',
+      'zod/no-number-schema-with-int': 'warn',
+      'zod/no-number-schema-with-is-finite': 'warn',
+      'zod/no-number-schema-with-is-int': 'warn',
+      'zod/no-number-schema-with-safe': 'warn',
+      'zod/no-number-schema-with-step': 'warn',
+      'zod/no-promise-schema': 'warn',
+      'zod/no-schema-with-is-nullable': 'warn',
+      'zod/no-schema-with-is-optional': 'warn',
+      'zod/no-string-schema-with-uuid': 'warn',
+      'zod/prefer-enum-over-literal-union': 'warn',
+      'zod/prefer-loose-object': 'warn',
+      'zod/prefer-meta-last': 'warn',
+      'zod/prefer-strict-object': 'warn',
+      'zod/prefer-top-level-string-formats': 'warn',
+      'zod/prefer-trim-before-string-length-checks': 'warn',
+    },
+  },
+]
+
+export default config
